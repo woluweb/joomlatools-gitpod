69e620551133340001384dfe83aa38608a50585c
Marc Dechèvre <marc@woluweb.be>
2021-05-20 16:16:54 +0200

    Update config.sh

--- .gitpod/config.sh
+++ .gitpod/config.sh
@@ -1,5 +1,4 @@
-#joomla_beta=4.0-dev
+joomla_beta=4.0-dev
 #joomla=3.8.9
 #repo=https://github.com/joomlatools/joomlatools-platform.git
-repo=https://github.com/joomla/joomla-cms/tree/15c0317afb8a9726d128c8c04a81fd50e8795863
 #composer="joomlatools/platform-media joomlatools/platform-finder joomlatools/platform-content joomlatools/platform-categories"
